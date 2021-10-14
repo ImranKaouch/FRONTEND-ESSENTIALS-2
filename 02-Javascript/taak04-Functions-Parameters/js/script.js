7d77d6947f610aa80df26c8c4b3f70dba8585a40
ImranKaouch <2122928@talnet.nl>
2021-10-14 11:54:48 +0200

    taak04

---
+++ 02-Javascript/taak04-Functions-Parameters/js/script.js
@@ -0,0 +1,11 @@
+
+
+getal1 = prompt("Voer een getal in");
+getal2 = prompt("Voer nogmaals een getal in");
+
+function telOp(getal, getal2){
+    let som = getal1 * getal2;
+    return som;
+}
+
+console.log(telOp(getal1, getal2));
