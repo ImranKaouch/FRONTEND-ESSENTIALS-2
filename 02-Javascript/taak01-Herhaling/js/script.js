94463bb702a272b5621e8905c7af264ac20346e3
ImranKaouch <2122928@talnet.nl>
2021-10-14 10:51:31 +0200

    taak01

--- 02-Javascript/taak01-Herhaling/js/script.js
+++ 02-Javascript/taak01-Herhaling/js/script.js
@@ -1,2 +1,19 @@
 let strNaam;
 let boolNaamKlopt;
+strNaam = prompt("Wat is uw naam?");
+alert ("Je heet " + strNaam);
+boolNaamKlopt = confirm("Heet je echt " + strNaam + "?")
+
+if(boolNaamKlopt == true){
+    alert("Welkom " + strNaam)
+}
+else{
+    prompt("Voer nogmaals uw naam in");
+}
+boolNaamKlopt = confirm("Heet je echt " + strNaam + "?")
+if(boolNaamKlopt == true){
+    alert("Welkom " + strNaam + "!");
+}
+else{
+    alert("Ok, evengoed Welkom")
+}
