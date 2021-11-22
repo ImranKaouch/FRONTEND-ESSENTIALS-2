376d2df705dab1ef807dc31b7dd4e9a960351fe3
ImranKaouch <2122928@talnet.nl>
2021-11-22 10:03:37 +0100

    taak06

--- 02-Javascript/taak06-Functions-Samenvatting/js/script.js
+++ 02-Javascript/taak06-Functions-Samenvatting/js/script.js
@@ -1,5 +1,7 @@
+
 var score = 0;
 let getal1;
+let getal2;
 
 function makeRandomNumber(){
     return Math.floor(Math.random()*99+1);
@@ -11,4 +13,25 @@ function showRandomNumber1(){
     return randomGetal;
 }
 
+function showRandomNumber2(){
+    let randomGetal = makeRandomNumber()
+    document.querySelector(".getal2").innerHTML = randomGetal;
+    return randomGetal;
+}
+
+function geefAntwoord(antwoord){
+    if(antwoord=="hoger"){
+        if(getal1>getal2){score++;  document.querySelector("body").style.backgroundColor = "Green";};
+        if(getal1<getal2){score--;  document.querySelector("body").style.backgroundColor = "Red";};
+    }
+    if(antwoord=="lager"){
+        if(getal1>getal2){score--; document.querySelector("body").style.backgroundColor = "Red";};
+        if(getal1<getal2){score++; document.querySelector("body").style.backgroundColor = "Green";}
+    }
+    document.querySelector(".score").innerHTML=score;
+    getal1 = showRandomNumber1();
+    getal2 = showRandomNumber2();
+}
+
 getal1 = showRandomNumber1();
+getal2 = showRandomNumber2();
