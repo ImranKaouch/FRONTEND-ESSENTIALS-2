7bbfff830cfc86474b5e94d13d6fb571ad9dfa10
ImranKaouch <2122928@talnet.nl>
2021-11-22 10:17:00 +0100

    taak07

---
+++ 02-Javascript/taak07-Arrays-Introductie/js/script.js
@@ -0,0 +1,16 @@
+
+topFilm = ["Endgame", "Harry Potter", "Pokerface"];
+topGames = ["Fifa", "Black Ops", "GTA V"];
+topMuziek = ["Boef", "Frenna", "Broederliefde"];
+
+console.log(topFilm[0]);
+console.log(topFilm[1]);
+console.log(topFilm[2]);
+
+console.log(topGames[0]);
+console.log(topGames[1]);
+console.log(topGames[2]);
+
+console.log(topMuziek[0]);
+console.log(topMuziek[1]);
+console.log(topMuziek[2]);
