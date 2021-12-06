8d143d35e3b8ed6e87d41a02d9a3055b8f9f2d07
ImranKaouch <2122928@talnet.nl>
2021-12-06 08:59:55 +0100

    taaak

--- 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
+++ 02-Javascript/taak09-Arrays-Toevoegen/js/script.js
@@ -10,14 +10,28 @@ topFilms = [
     "One Flew Over the Cuckoo's Nest",
     "Lawrence of Arabia"
 ];
-
-eigenTopFilms = [];
+eigenTopFilms = [
+    
+]
 
 //met deze for-loop word de ordered list gevuld met film namen
 for(i = 0; i < topFilms.length; i++){
     document.querySelector(".imdb-film" + (i+1)).innerHTML = topFilms[i];
 }
+var numkeus = 1;
 
 function kiesFilm(){
-
+    var element = document.querySelector('.eigen-film' + numkeus); 
+    var keuze = prompt("Welke film staat op plek " +numkeus + "?")
+    element.innerHTML = topFilms[keuze-1];
+    numkeus++;
+    document.querySelector('.kiesfilm-button').innerHTML = "Kies film voor plek " + numkeus; 
 }
+
+function showAlert() {
+    if (numkeus > 3) {
+        alert("Top 3 is gekozen");
+    }
+    showAlert();
+}
+
